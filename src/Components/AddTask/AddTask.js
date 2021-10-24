dbe3c6a6f43fa493af4b4e1d2f6ad302d2d01c36
manel gharbi <manelgharbi23@gmail.com>
2021-10-24 20:57:58 +0200

    first commit

---
+++ src/Components/AddTask/AddTask.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTask } from "../../JS/Actions/listTasks";
+import { Form, Button } from "react-bootstrap";
+import "./AddTask.css";
+
+const AddTask = () => {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+const handleChange = (e) => {
+    e.preventDefault();
+    if (text) {
+      dispatch(addTask({ id: Math.random(), text: text, isDone: false }));
+      setText("");
+    } else {
+      alert("cant add an empty task");
+    }
+  };
+
+  return (
+    <Form onSubmit={handleChange} className="add-task-container">
+      <Form.Control
+        type="text"
+        placeholder="Enter task title ..."
+        className="input-task"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <Button variant="light" onClick={handleChange}>
+        Add
+      </Button>
+    </Form>
+  );
+};
+
+export default AddTask;
---
+++ src/Components/Edit/Edit.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editTask } from "../../JS/Actions/listTasks";
+import { Modal, Button } from "react-bootstrap";
+
+const Edit = ({ task }) => {
+  const [show, setShow] = useState(false);
+  const [newText, setnewText] = useState(task.text);
+  const dispatch = useDispatch();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleEdit = () => {
+    dispatch(editTask(task.id, newText));
+    handleClose();
+  };
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Edit
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <input value={newText} onChange={(e) => setnewText(e.target.value)} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleEdit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default Edit;
